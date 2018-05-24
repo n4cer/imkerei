85f097bde5c60299f5c2ba7eb1a6658bcb0b39c9
n4cer <mm.dehler@gmail.com>
2018-05-24 08:11:56 +0200

    DSGVO

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -37,4 +37,18 @@ public class Application extends Controller {
     
     return ok(imprint.render(name, street, city, email_encoded));
   }
+  
+  public Result privacy() {
+    String name = configuration.underlying().getString("owner.name");
+    String street = configuration.underlying().getString("owner.street");
+    String city = configuration.underlying().getString("owner.city");
+    String country = configuration.underlying().getString("privacy.country");
+    String email = configuration.underlying().getString("privacy.email");
+    String email_encoded = "";
+    for (int i = 0; i < email.length(); i++) {
+      email_encoded += ("&#" + email.codePointAt(i) + ";");
+    }
+    
+    return ok(privacy.render(name, street, city, country, email_encoded));
+  }
 }
